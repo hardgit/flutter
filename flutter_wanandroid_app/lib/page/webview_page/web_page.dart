8557e60945fd3d98ce7e0caef361a1b9fd2ae265
zhaofeng <2297990456@qq.com>
2022-01-18 15:59:16 +0800

    web页title转义字符

--- flutter_wanandroid_app/lib/page/webview_page/web_page.dart
+++ flutter_wanandroid_app/lib/page/webview_page/web_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_wanandroid_app/base/get_common_view.dart';
  import 'package:flutter_wanandroid_app/res/style.dart';
+import 'package:html/parser.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 import 'web_controller.dart';
@@ -30,7 +31,10 @@ class WebViewPage extends GetCommonView<WebController>{
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(controller.entity.title,style: Styles.style_black_16,),
+        title: Text(
+          parse(controller.entity.title).body!.text,
+          style: Styles.style_black_16,
+        ),
       ),
       body: Column(
         children: [
